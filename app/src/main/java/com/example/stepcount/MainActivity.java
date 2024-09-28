8dd5a866d6a5995f8c2bd7d70cfdc69f60d78582
Kolovratik <Kolovratik@HOME-PC>
2024-09-28 12:01:34 +0300

    Выполненная практическая работа на оценку удовлетворительно

--- app/src/main/java/com/example/stepcount/MainActivity.java
+++ app/src/main/java/com/example/stepcount/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stepcount;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
     public boolean Active = false;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     int count = 0;
     TextView tvResult;
     Button btnPause;
-    long lastTimeUpdate;
+    long LastTimeUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.registerListener((SensorEventListener) this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_FASTEST);
-        lastTimeUpdate = System.currentTimeMillis();
+        LastTimeUpdate = System.currentTimeMillis();
     }
 
     @Override
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.registerListener((SensorEventListener) this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_FASTEST);
-        lastTimeUpdate = System.currentTimeMillis();
+        LastTimeUpdate = System.currentTimeMillis();
     }
 
     @Override
@@ -57,5 +58,30 @@ public class MainActivity extends AppCompatActivity {
         Active = !Active;
         if(Active) btnPause.setText("ПАУЗА");
         else btnPause.setText("ВОЗОБНОВИТЬ");
+    }
+
+    public void onSensorChanged(SensorEvent sensorEvent){
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float X = sensorEvent.values[0];
+            float Y = sensorEvent.values[1];
+            float Z = sensorEvent.values[2];
+
+            float AccelarationSquareRoot =  (X*X+Y*Y+Z*Z)
+                    / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+            long ActualTimeUpdate = System.currentTimeMillis();
+
+            if(AccelarationSquareRoot >= 2){
+                if(ActualTimeUpdate - LastTimeUpdate < 200) return;;
+                if(!Active) return;
+                LastTimeUpdate = ActualTimeUpdate;
+                count++;
+                tvResult.setText(String.valueOf(count));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
